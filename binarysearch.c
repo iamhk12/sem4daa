2efb58c8d5fd8551ab777ca627241a7b43025904
Himanshu Kothari <hkothari247@gmail.com>
2023-04-24 23:12:56 +0530

    Update binarysearch.c

--- binarysearch.c
+++ binarysearch.c
@@ -1,26 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-void main(){
-   int array[7];
-   int target,i;
+void main()
+{
+
+   int n;
+   bool elementFound = false;
+   printf("Enter the size of the sorted array : ");
+   scanf("%d", &n);
+   int array[n];
+   int target, i;
    printf("Enter  the elements\n");
-   for(i=0;i<7;i++){
-      scanf("%d",&array[i]);
+   for (i = 0; i < n; i++)
+   {
+      scanf("%d", &array[i]);
    }
    printf("Enter the target\n");
-   scanf("%d",&target);
+   scanf("%d", &target);
 
-   int start=0;
-   int end =sizeof(array)/sizeof(array[0]) -1;
-   while(start<=end){
-     int mid =start+end/2;
-     if(array[mid]==target){
-        printf("Element found at index %d",mid);
-        break;
-     }else if(array[mid]<target){
-        start=mid+1;
-     }else if(array[mid]>target){
-        end=mid-1;
-     }
+   int start = 0;
+   int end = sizeof(array) / sizeof(array[0]) - 1;
+   while (start <= end)
+   {
+      int mid = start + end / 2;
+      if (array[mid] == target)
+      {
+         printf("Element found at index %d", mid);
+         elementFound = true;
+         break;
+      }
+      else if (array[mid] < target)
+      {
+         start = mid + 1;
+      }
+      else if (array[mid] > target)
+      {
+         end = mid - 1;
+      }
    }
+
+   if (!elementFound)
+      printf("Element not present in array");
 }
